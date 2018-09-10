3cda0e7df9213c0d1d3b05bbde8dab9ee4b2b7f0
remdd <robin.ed.marsden@gmail.com>
2018-09-10 16:22:08 +0100

    Basic badge zooming operation implemented

--- js/badges.js
+++ js/badges.js
@@ -14,19 +14,20 @@ $(function() {
 	var xmlPath_Attractor = "xml/attractor.xml";
 	var $xmlDoc_Attractor;
 	var badgeArray = [];
-	var displayedBadge;
+	var selectedBadge;
 	var folderPng, folderJson;
 
 	//	Timers & handlers
 	var stillThereTimeMax, stillThereTime, inactivityTimerMax;
 	var stillThereHandler, inactivityHandler;
-	var focusedBadge;
+	var selectedBadge;
 	var canvasColors = [];
 
 	//	Menu badge animation looping
 	var menuBadgeAnimHandler;
 	var menuBadgeAnimTimeMultiplier = 5000;
-	var menuBadgeAnimTimeMin = 1000;
+	var menuBadgeAnimTimeMin = 3000;
+	var badgeAnimating;
 
 	//	Called on first load
 	function firstLoad() {
@@ -86,26 +87,25 @@ $(function() {
 
 		folderPng = $xml.find('folderPng').text();
 		folderJson = $xml.find('folderJson').text();
+		folderOutlines = $xml.find('folderOutlines').text();
 
 		//	Load XML slide data to slidesArray
 		$xml.find('badge').each(function(){
 			//	Build up slide variable
 			var $thisBadge = $(this);
+			console.log($thisBadge.attr('ref'));
 			var thisBadgeObject = {
-				ref: $thisBadge.find('ref').html(),
+				ref: $thisBadge.attr('ref'),
 				name: $thisBadge.find('name').html(),
 				objectID: $thisBadge.find('objectID').html(),
-				date: $thisBadge.find('date').html(),
+				description: $thisBadge.find('description').html(),
 				type: $thisBadge.find('type').html(),
-				materials: $thisBadge.find('materials').html(),
-				measurements: $thisBadge.find('measurements').text(),
-				description: {
-					paragraph1: $thisBadge.find('description paragraph1').html() === undefined ? undefined : $thisBadge.find('description paragraph1').html(),
-					paragraph2: $thisBadge.find('description paragraph2').html() === undefined ? undefined : $thisBadge.find('description paragraph2').html(),
-					paragraph3: $thisBadge.find('description paragraph3').html() === undefined ? undefined : $thisBadge.find('description paragraph3').html(),
-					paragraph4: $thisBadge.find('description paragraph4').html() === undefined ? undefined : $thisBadge.find('description paragraph4').html(),
-					paragraph5: $thisBadge.find('description paragraph5').html() === undefined ? undefined : $thisBadge.find('description paragraph5').html(),
-				},
+				launched: $thisBadge.find('launched').html(),
+				builder: $thisBadge.find('builder').html(),
+				length: $thisBadge.find('length').html(),
+				speed: $thisBadge.find('speed').html(),
+				crew: $thisBadge.find('crew').html(),
+				career: $thisBadge.find('career').html(),
 				json: {
 					file: $thisBadge.find('json file').text(),
 					type: $thisBadge.find('json type').text()
@@ -120,7 +120,7 @@ $(function() {
 		badgeArray.sort(compare);
 		console.log(badgeArray);
 		addBadges();
-		cycleMenuBadgeAnimations();
+		// cycleMenuBadgeAnimations();
 	}
 
 	function compare(a, b) {
@@ -138,36 +138,160 @@ $(function() {
 	function addBadges() {
 		//	Add menu SVG images
 		badgeArray.forEach(function(badge) {
+			//	Add badge to correct row in div per first character of badge ref
 			var row = "row" + badge.ref.substr(0,1);
 			$('#' + row).append('<div data-ref="' + badge.ref + '" class="badge ' + badge.json.type + '">');
 			badge.anim = bodymovin.loadAnimation({
 				container: document.querySelectorAll('[data-ref="' + badge.ref + '"]')[0],
 				renderer: 'svg',
-				loop: false,
+				loop: true,
 				autoplay: false,
 				path: folderJson + '/' + badge.json.file
 			});
-			$('*[data-ref="' + badge.ref + '"]').click(function() {
-				$('*[data-ref="' + badge.ref + '"]').addClass('highlight');
-				badge.anim.play();
-				console.log(badge.name);
-			});
-			badge.anim.addEventListener('complete', function() {
+		});
+		$('.badge').click(function() {
+			console.log("Selecting badge");
+			console.log(selectedBadge);
+			selectBadge(getBadge($(this).attr('data-ref')));
+		});
+	}
+
+	function selectBadge(badge) {
+		selectedBadge = badge;
+		populatePopupText(badge);
+		startAnim(badge, function() {
+			focusBadge(badge);
+		});
+	}
+
+	function startAnim(badge, callback) {
+		badgeAnimating = true;
+		$badge = $('*[data-ref="' + badge.ref + '"]');
+		$badge.addClass('highlight zoomed onTop');
+		$badge.one('transitionend', function() {
+			badge.anim.play();
+			console.log("Animating " + badge.name);
+			badge.anim.addEventListener('loopComplete', function() {
+				badge.anim.removeEventListener('loopComplete');
 				badge.anim.stop();
-				$('*[data-ref="' + badge.ref + '"]').removeClass('highlight');
-			})
+				console.log("Animation complete");
+				if(callback) {
+					callback();
+				}
+			});
+		});
+	}
+
+	function focusBadge(badge) {
+		//	Append the colour and outline png representation of the selected badge to the overlay container and position
+		createZoomImgs(badge);
+		revertJson(false, badge, function() {
+			$('.pngImg').addClass('focused');
+			var styles = {
+				'left': '150px',
+				'top': '180px',
+				'width': '720px',
+				'height': '720px'
+			}
+			$('.pngImg').css(styles);
+			$('#colorImg').one('transitionend', function() {
+				$('#colorImg').detach().appendTo('#popupDiv');
+				$('#outlineImg').remove();
+			});
+			$('#popupDiv').addClass('displayed');
+			hideMenuScreen();
+		});
+	}
+
+	function revertJson(instant, badge, callback) {
+		$badge = $('*[data-ref="' + badge.ref + '"]');
+		if(instant) {
+			$badge.addClass('noTransition');
+			$badge.removeClass('highlight zoomed onTop');
+			flushCss($badge.get(0));
+			$badge.removeClass('noTransition');
+		} else {
+			$badge.removeClass('highlight zoomed onTop');
+		}
+		$badge.one('transitionend', function() {
+			if(callback) {
+				callback();
+			}
+		});
+	}
+
+	function createZoomImgs(badge) {
+		var $originalJsonImg = $('*[data-ref="' + badge.ref + '"]');
+		var marginLeft = parseInt($originalJsonImg.css('marginLeft'));
+		var marginTop = parseInt($originalJsonImg.css('marginTop'));
+		var styles = {
+			'left': $originalJsonImg.position().left + marginLeft, 
+			'top': $originalJsonImg.position().top + marginTop,
+			'height': $originalJsonImg.height(),
+			'width': $originalJsonImg.width()
+		}
+		var colorImg = '<div id="colorImg" class="pngImg zoomed onTop" style="background-image: url(' + "'" + folderPng + '/' + badge.png.file + "'" + '"></img>'
+		var outlineImg = '<div id="outlineImg" class="pngImg zoomed onTop" style="background-image: url(' + "'" + folderOutlines + '/' + badge.png.file + "'" + '"></img>'
+		// $('#popupImageDiv').append(colorImg);
+		$('#popupImageDiv').append(outlineImg);
+		$('#popupImageDiv').append(colorImg);
+		$('.pngImg').css(styles);
+		if($originalJsonImg.hasClass('circleBig')) {
+			$('.pngImg').addClass('big');
+		} else if($originalJsonImg.hasClass('circleMedium') || $originalJsonImg.hasClass('diamondMedium') || $originalJsonImg.hasClass('shieldMedium')) {
+			$('.pngImg').addClass('medium');
+		} else {
+			$('.pngImg').addClass('small');
+		}
+		// flushCss($('.pngImg').get(0));
+	}
+
+	function hideFocusBtns() {
+	}
+
+	function hideMenuScreen() {
+		$('#sidebarContent').fadeOut('slow');
+		$('#menuBadges').fadeOut('slow');
+		$('#infoPane').fadeOut('slow');
+		$('.arrowBtn').addClass('shown');
+		$('.arrowBtn').removeClass('hidden');
+		$('.arrowBtn').one('transitionend', function() {
+			$('.arrowBtn').removeClass('shown');
+		});
+	}
+
+	function backToMenu() {
+		selectedBadge = undefined;
+		$('#sidebarContent').fadeIn('slow');
+		$('#menuBadges').fadeIn('slow');
+		$('#infoPane').fadeIn('slow');
+		$('.arrowBtn').addClass('hidden');
+		$('.arrowBtn').one('transitionend', function() {
+			$('.arrowBtn').removeClass('shown');
+		});
+		$('#popupDiv').removeClass('displayed');
+		$('#popupDiv').one('transitionend', function() {
+			console.log("Removing PNG images");
+			$('.pngImg').remove();
 		})
 	}
 
-	function cycleMenuBadgeAnimations() {
-		var time = Math.floor(Math.random() * menuBadgeAnimTimeMultiplier + menuBadgeAnimTimeMin);
-		var nextBadge = badgeArray[Math.floor(Math.random() * badgeArray.length)];
-		menuBadgeAnimHandler = setTimeout(function() {
-			nextBadge.anim.play();
-			cycleMenuBadgeAnimations();
-		}, time);
+	function populatePopupText(badge) {
+		console.log("Populating text...");
+		$('#textName').html(badge.name);
+		$('#textDescription').html(badge.description);
+		if(badge.type === 'shore station') {
+			//	Handle shore stations
+		} else {
+			$('#textType').html(badge.type);
+			$('#textLength').html(badge.length);
+			$('#textBuilder').html(badge.builder);
+			$('#textLaunched').html(badge.launched);
+			$('#textSpeed').html(badge.speed);
+			$('#textCrew').html(badge.crew);
+			$('#textCareer').html(badge.career);
+		}
 	}
-
 
 
 	function stopMenuBadgeAnimations() {
@@ -223,8 +347,9 @@ $(function() {
 	function showAttractScreen(callback) {
 		console.log("Showing Attract screen");
 		clearInactivityTimer();
+		stopMenuBadgeAnimations();
 		$attractScreen.fadeIn('slow', function() {
-			showMenuScreen();
+			backToMenu();
 			if(callback && typeof(callback) === 'function') {
 				callback();
 			}
@@ -259,9 +384,7 @@ $(function() {
 		return badge;
 	}
 
-	function showMenuScreen() {
 
-	}
 
 	function zoomBadge(ref) {
 
@@ -278,6 +401,7 @@ $(function() {
 	//	Event handlers
 	$attractScreen.click(function() {
 		$attractScreen.fadeOut('slow', function() {
+			// cycleMenuBadgeAnimations();
 			restartInactivityTimer();
 		});
 	});
@@ -303,10 +427,20 @@ $(function() {
 	});
 
 	$('.backToMenuBtn').click(function() {
-		showMenuScreen();
+		backToMenu();
 	});
 
 
+	// function cycleMenuBadgeAnimations() {
+	// 	var time = Math.floor(Math.random() * menuBadgeAnimTimeMultiplier + menuBadgeAnimTimeMin);
+	// 	var badge = badgeArray[Math.floor(Math.random() * badgeArray.length)];
+	// 	menuBadgeAnimHandler = setTimeout(function() {
+	// 		if(!badgeAnimating) {
+	// 			startAnim(badge);
+	// 		}
+	// 		cycleMenuBadgeAnimations();
+	// 	}, time);
+	// }
 
 	firstLoad();
 
